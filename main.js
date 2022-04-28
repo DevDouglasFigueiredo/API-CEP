a7738a419f22af32a29e3d3b8b2a532c1cf2b0e9
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-04-28 17:53:29 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+
+const preencherFormulario = (endereco) => {
+    
+    document.querySelector('#endereco').value = endereco.logradouro;
+    document.querySelector('#bairro').value = endereco.bairro;
+    document.querySelector('#cidade').value = endereco.localidade;
+    document.querySelector('#estado').value = endereco.uf;
+
+}
+
+const limparFormulario = (endereco) => {
+    document.querySelector('#endereco').value = "";
+    document.querySelector('#bairro').value ="";
+    document.querySelector('#cidade').value = "";
+    document.querySelector('#estado').value = "";
+
+}
+
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const cepValido = (cep) => cep.length == 8 &&  eNumero(cep);
+
+
+const pesquisarCep = async () =>{
+    limparFormulario();
+    const cep = document.querySelector('#cep').value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    if(cepValido(cep)){
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        if(endereco.hasOwnProperty('erro')){
+            document.querySelector('#endereco').value ='CEP`não encontrado';
+    
+        } else{
+    
+            preencherFormulario(endereco);
+        }
+        
+    } else {
+        document.querySelector('#endereco').value ='CEP incorrreto';
+    }
+
+}
+
+
+document.querySelector('[data-cep]').addEventListener('focusout', pesquisarCep)
